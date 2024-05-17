57857ffd38b6ac9d0890cbedc6ad859c995a0f26
MatteoRuffa <cortese_17@protonmail.com>
2024-05-17 17:39:54 +0200

    added BONUS

---
+++ resources/views/prova.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Prova')
+
+@section('content')
+<main>
+    <p>Con piena consapevolezza delle mie capacità cognitive, affermo con fermezza 
+        e chiarezza: io {{$name}} {{$surname}}, in questo momento, dichiaro: </p>
+    <p>"Onestament a-i capiss nen tant, i speto l'àutre lezion".</p>
+</main>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/prova', function () {					
+        return view('Prova', ['name' => 'Matteo', 'surname' => 'R***a']);	
+});
