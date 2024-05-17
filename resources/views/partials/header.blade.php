fad90e55528754915a833dd3f5b9147919e1c885
MatteoRuffa <cortese_17@protonmail.com>
2024-05-17 17:42:18 +0200

    finished

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
             <ul class="">
                 <li><a href="#">prova</a></li>
                 <li><a href="#">prova</a></li>
-                <li><a href="#">prova</a></li>
+                <li><a href="#">E anca përò a mi g'andé nen ël menù përò a l'andrìa nen a prènd ij SCSS.</a></li>
                 <li><a href="#">prova ancora</a></li>
                 <li><a href="#">Boja Fàuss con coste próve</a></li>
                 <li><a href="#">Adess giuro che la finisso</a></li>
