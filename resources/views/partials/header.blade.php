0717abd4bc3086e2271745ab8ff760fcd87a7852
MatteoRuffa <cortese_17@protonmail.com>
2024-05-17 17:25:09 +0200

    added an unsuccessful attempt

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,15 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+
+<div class="mr-container">
+    <div class="logo-container">
+        <div>
+            <ul class="">
+                <li><a href="#">prova</a></li>
+                <li><a href="#">prova</a></li>
+                <li><a href="#">prova</a></li>
+                <li><a href="#">prova ancora</a></li>
+                <li><a href="#">Boja Fàuss con coste próve</a></li>
+                <li><a href="#">Adess giuro che la finisso</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
